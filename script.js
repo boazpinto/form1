445a0533504e48d83c775358e7f684ff7e638e08
boazpinto <53555555+boazpinto@users.noreply.github.com>
2019-08-14 13:10:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function previewPic(){
    document.getElementById("preview").src=URL.createObjectURL(document.getElementById("file").files[0]);
    // document.getElementById("preview").src="no name.jpg"
 }
+function reset(){
+
+    document.getElementById("preview").src="no name.jpg"
+}
+
+
